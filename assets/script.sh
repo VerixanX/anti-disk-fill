e3656d8ee6cab0b978f8011fb64e5f4bc1c2703f
Elias <163117122+Elias-Lindholm@users.noreply.github.com>
2024-08-17 16:10:18 +0200

    Update script.sh

--- assets/script.sh
+++ assets/script.sh
@@ -1,17 +1,19 @@
-#! /bin/bash
+#!/bin/bash
+
+FOLDER=/var/lib/pterodactyl/volumes
+MAXSIZEINGIGS=5  # Changed to 5GB
+
+# Convert GB to KB (1GB = 1024*1024 KB)
+MAXSIZE=$(( $MAXSIZEINGIGS * 1024 * 1024 ))
 
 while true
 do
-FOLDER=/var/lib/pterodactyl/volumes
+    FOLDERSIZE=$(du -ks "$FOLDER" | cut -f1)
 
-MAXSIZEINGIGS=32
+    if [ $FOLDERSIZE -gt $MAXSIZE ]
+    then
+        rm -r "$FOLDER"/*
+    fi
 
-MAXSIZE=$(( $MAXSIZEINGIGS * 51200 * 51200 ))
-FOLDERSIZE=`du -ks $FOLDER | cut -f1`
-
-if [ $FOLDERSIZE -gt $MAXSIZE ]
-then
-        rm -r $FOLDER/*
-fi
-sleep 1
+    sleep 10
 done
